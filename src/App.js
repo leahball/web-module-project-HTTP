affedc0dc3e0635e4b6c29eb0d29d9131f96bbb1
iamleahb <leah.ball@gmail.com>
2022-01-26 19:52:49 -0600

    added use effect to Edit Movie

--- src/App.js
+++ src/App.js
@@ -1,61 +1,61 @@
 import React, { useEffect, useState } from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
 
-import FavoriteMovieList from './components/FavoriteMovieList';
+import FavoriteMovieList from "./components/FavoriteMovieList";
 
-import axios from 'axios';
+import axios from "axios";
+import EditMovieForm from "./components/EditMovieForm";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
-  useEffect(()=>{
-    axios.get('http://localhost:9000/api/movies')
-      .then(res => {
+  useEffect(() => {
+    axios
+      .get("http://localhost:9000/api/movies")
+      .then((res) => {
         setMovies(res.data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  const deleteMovie = (id)=> {
-  }
+  const deleteMovie = (id) => {};
 
-  const addToFavorites = (movie) => {
-    
-  }
+  const addToFavorites = (movie) => {};
 
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" > HTTP / CRUD Module Project</span>
+        <span className="navbar-brand"> HTTP / CRUD Module Project</span>
       </nav>
 
       <div className="container">
-        <MovieHeader/>
+        <MovieHeader />
         <div className="row ">
-          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
-        
+          <FavoriteMovieList favoriteMovies={favoriteMovies} />
+
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm />
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie />
             </Route>
 
             <Route path="/movies">
-              <MovieList movies={movies}/>
+              <MovieList movies={movies} />
             </Route>
 
             <Route path="/">
-              <Redirect to="/movies"/>
+              <Redirect to="/movies" />
             </Route>
           </Switch>
         </div>
@@ -64,6 +64,4 @@ const App = (props) => {
   );
 };
 
-
 export default App;
-
--- src/mocks/api.js
+++ src/mocks/api.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const Data = require('./data');
-const cors = require('cors');
+const express = require("express");
+const Data = require("./data");
+const cors = require("cors");
 
 const api = express();
 
@@ -8,30 +8,30 @@ api.use(express.json());
 
 api.use(cors());
 
-api.get('/api/movies', (req, res) => {
-  res.json(Data.getAll())
-})
+api.get("/api/movies", (req, res) => {
+  res.json(Data.getAll());
+});
 
 //Get Post Endpoint
-api.get('/api/movies/:id',(req, res) => {
-  res.json(Data.getById(req.params.id))
-})
+api.get("/api/movies/:id", (req, res) => {
+  res.json(Data.getById(req.params.id));
+});
 
 //Create Post Endpoint
-api.post('/api/movies', (req, res) => {
-  res.json(Data.create(req.body))
-})
+api.post("/api/movies", (req, res) => {
+  res.json(Data.create(req.body));
+});
 
 //Edit Post Endpoint
-api.put('/api/movies/:id', (req, res) => {
-  res.json(Data.edit(req.params.id, req.body))
-})
+api.put("/api/movies/:id", (req, res) => {
+  res.json(Data.edit(req.params.id, req.body));
+});
 
 //Delete Post Endpoint
-api.delete('/api/movies/:id', (req, res) => {
-  res.json(Data.remove(req.params.id))
-})
+api.delete("/api/movies/:id", (req, res) => {
+  res.json(Data.remove(req.params.id));
+});
 
 api.listen(9000, () => {
-  console.log('listening on 9000')
-})
+  console.log("listening on 9000");
+});
